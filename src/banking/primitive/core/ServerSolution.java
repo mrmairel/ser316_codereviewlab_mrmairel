4b501a3c3dfc639737d965a65410435e400e09d5
mrmairel <mrmairel@asu.edu.(none)>
2017-04-08 19:33:32 -0700

    Fix stylistic issues with if statements, bug#3

--- src/banking/primitive/core/ServerSolution.java
+++ src/banking/primitive/core/ServerSolution.java
@@ -27,8 +27,9 @@ class ServerSolution implements AccountServer {
 				int size = sizeI.intValue();
 				for (int i=0; i < size; i++) {
 					Account acc = (Account) in.readObject();
-					if (acc != null)
+					if (acc != null){
 						accountMap.put(acc.getName(), acc);
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -48,7 +49,9 @@ class ServerSolution implements AccountServer {
 	private boolean newAccountFactory(String type, String name, float balance)
 		throws IllegalArgumentException {
 		
-		if (accountMap.get(name) != null) return false;
+		if (accountMap.get(name) != null) {
+			return false;
+		}
 		
 		Account acc;
 		if ("Checking".equals(type)) {
@@ -71,7 +74,9 @@ class ServerSolution implements AccountServer {
 	public boolean newAccount(String type, String name, float balance) 
 		throws IllegalArgumentException {
 		
-		if (balance < 0.0f) throw new IllegalArgumentException("New account may not be started with a negative balance");
+		if (balance < 0.0f) {
+			throw new IllegalArgumentException("New account may not be started with a negative balance");
+		}
 		
 		return newAccountFactory(type, name, balance);
 	}
